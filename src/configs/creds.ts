593ae3e192a3da214714750b39c9c0ade6244568
Akshay <akshay.kumar@eazyerp.com>
2026-01-30 16:20:24 +0530

    Fix

--- src/configs/creds.ts
+++ src/configs/creds.ts
@@ -12,7 +12,7 @@ export namespace App{
     export const COOKIE_OPTIONS = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     } as CookieOptions;
 }
