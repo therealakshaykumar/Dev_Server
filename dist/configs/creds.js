f535e1e101cbc5562ef514e6f3eb4e9d8b9e8564
Akshay <akshay.kumar@eazyerp.com>
2026-02-04 22:27:11 +0530

    Hotfix

--- dist/configs/creds.js
+++ dist/configs/creds.js
@@ -12,7 +12,7 @@ export var App;
     const isProduction = App.NODE_ENV === 'production';
     App.COOKIE_OPTIONS = {
         httpOnly: true,
-        secure: isProduction,
+        secure: false,
         sameSite: "none",
         path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
